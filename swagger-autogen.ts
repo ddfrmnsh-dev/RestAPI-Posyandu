156ac90505d19bf759634bfe852c6285b869b2cf
Dede Firmansah <dedefirmansah0508@gmail.com>
2023-11-20 15:39:52 +0700

    update

--- swagger-autogen.ts
+++ swagger-autogen.ts
@@ -1,4 +1,4 @@
-const swaggerAutogen = require('swagger-autogen')();
+const swaggerAutogen = require('swagger-autogen');
 
 const outputFile = './swagger.json';
 const endpointsFiles = ['./src/routes/*.ts'];
